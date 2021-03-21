2b2faef33642fe4f9b4770f2e8a15506ec455b7b
HarrussHo <harrussho@gmail.com>
2021-03-21 13:19:02 +0800

    21/03/2021 Tech with Tim Python + Django Lesson 2

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,10 @@
+from django.db.models import fields
+from api.models import Room
+from rest_framework import serializers
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
